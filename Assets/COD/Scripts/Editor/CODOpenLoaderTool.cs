19b433821bd08cc1d8453a7039162dd21abdf4ac
assafsahar <assaf@codactive.com>
2023-08-02 22:41:31 +0300

    Added editor tool to run the game (after loading the loader scene automatically) or edit the game

---
+++ Assets/COD/Scripts/Editor/CODOpenLoaderTool.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class CODOpenLoaderTool
+{
+    public class OpenLoaderTool
+    {
+        [MenuItem("COD/PlayGame")]
+        //[System.Obsolete]
+        public static void OpenLoader()
+        {
+            string currentSceneName = "GameScene";
+            File.WriteAllText(".lastScene", currentSceneName);
+            EditorSceneManager.OpenScene($"{Directory.GetCurrentDirectory()}/Assets/COD/Scenes/Loader.unity");
+            EditorApplication.isPlaying = true;
+        }
+
+        [MenuItem("COD/LoadEditedScene")]
+        public static void ReturnToLastScene()
+        {
+            string lastScene = File.ReadAllText(".lastScene");
+            EditorSceneManager.OpenScene($"{Directory.GetCurrentDirectory()}/Assets/COD/Scenes/{lastScene}.unity");
+        }
+
+    }
+}
