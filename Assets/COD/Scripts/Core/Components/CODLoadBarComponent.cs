6209478bdbf5e0b8e2d33cead7b8248db0b60a96
assafsahar <assaf@codactive.com>
2023-11-27 09:45:11 +0200

    added some UI sprites, added loadbar component

---
+++ Assets/COD/Scripts/Core/Components/CODLoadBarComponent.cs
@@ -0,0 +1,68 @@
+using DG.Tweening;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace COD.Core
+{
+    public class CODLoadBarComponent : CODMonoBehaviour
+    {
+        [SerializeField] private Image loadingImage;
+        [SerializeField] private TMP_Text loaderNumber;
+        [SerializeField] private float fillSpeed = 1;
+        [SerializeField] float animationDuration = 1.0f;
+
+
+        private float targetAmount = 0;
+        private int currentAmount = 0;
+        private bool isAnimating = false;
+
+        private void Awake()
+        {
+            loadingImage.fillAmount = 0;
+            UpdateView();
+        }
+
+        public void SetTargetAmount(float amount)
+        {
+            DOTween.KillAll();
+            if (isAnimating)
+            {
+                StopAllCoroutines();
+            }
+            targetAmount = amount;
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            Debug.Log(targetAmount);
+            loadingImage.DOFillAmount(targetAmount / 100, fillSpeed).SetEase(Ease.Linear);
+            StartCoroutine(AnimateToTargetAmount((int)targetAmount));
+        }
+
+        private IEnumerator AnimateToTargetAmount(int targetAmount)
+        {
+            isAnimating = true;
+            float elapsedTime = 0.0f;
+            int startAmount = currentAmount;
+
+            while (elapsedTime < animationDuration)
+            {
+                elapsedTime += Time.deltaTime;
+
+                float t = Mathf.Clamp01(elapsedTime / animationDuration);
+                currentAmount = Mathf.RoundToInt(Mathf.Lerp(startAmount, targetAmount, t));
+                loaderNumber.text = currentAmount.ToString("N0") + "%"; ;
+                yield return null;
+            }
+
+            currentAmount = targetAmount;
+            loaderNumber.text = currentAmount.ToString("N0") + "%";
+            isAnimating = false;
+            Debug.Log($"from IEnumerator, targetAmount = {targetAmount} and currentAmount={currentAmount}");
+        }
+
+    }
+}
--- Assets/COD/Scripts/Core/Loaders/CODGameLoader.cs
+++ Assets/COD/Scripts/Core/Loaders/CODGameLoader.cs
@@ -10,28 +10,28 @@ namespace COD.Core
     public class CODGameLoader : CODGameLoaderBase
     {
         [SerializeField] private CODGameLoaderBase gameLogicLoader;
-        //[SerializeField] private CODLoadBarComponent loadbarComponent;
+        [SerializeField] private CODLoadBarComponent loadbarComponent;
         private void Start()
         {
-            //loadbarComponent.SetTargetAmount(20);
+            loadbarComponent.SetTargetAmount(20);
             WaitForSeconds(0.1f, DelayStart);
         }
 
         private void DelayStart()
         {
             var manager = new CODManager();
-            //loadbarComponent.SetTargetAmount(40);
+            loadbarComponent.SetTargetAmount(40);
             manager.LoadManager(() =>
             {
                 Debug.Log("LoadManager");
                 WaitForSeconds(0.1f, () =>
                 {
-                    //loadbarComponent.SetTargetAmount(98);
+                    loadbarComponent.SetTargetAmount(98);
                     gameLogicLoader.StartLoad(() =>
                     {
                         WaitForSeconds(0.1f, () =>
                         {
-                            //loadbarComponent.SetTargetAmount(100);
+                            loadbarComponent.SetTargetAmount(100);
                             SceneManager.LoadScene(1);
                             Destroy(this.gameObject);
                         });
